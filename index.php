1f4d44dc6a9690d746c34bfa538e2d1dfe5379cf
Deepika Patel <deepika.patel@ucertify.com>
2014-03-28 13:21:21 +0530

    display table

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
  <?php
- echo "this is testing.";
- echo "this is testing.";
- echo "this is testing.";
- echo "this is testing.";
- echo "this is testing.";
+
+function dispalyTable($i){
+	echo "Table of ". $i . " :";
+	for($j = 1; $j <= 10; $j++){
+		echo $j . " * " . $i . " = ". $j * $i ."<br />";
+	}
+}
+dispalyTable(5);
  ?>
